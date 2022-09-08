945f86e7e93dd220326a4a7c717d0d6bb0e7ef55
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2022-09-08 10:10:51 +0300

    add tf files for s3 example

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # aws-terraform-examples
 Simple examples for infra provisioning on AWS (WIP)
+
+## s3-example
+
+This is inspired from https://github.com/mydevopscoach/terraform-on-aws-repo/tree/main/s3
---
+++ s3-example/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "The AWS region to use to create resources."
+  default     = "us-central-1" # Frankfurt region
+}
+
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+    default     = "test-s3bucket-"
+}
+
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
+
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
